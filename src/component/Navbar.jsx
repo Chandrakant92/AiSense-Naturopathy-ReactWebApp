6199b3fd43c111f1049f82fcefdc6818de245aaf
Chandrakant <chandrakantgawali92@gmail.com>
2023-07-24 15:38:55 +0530

    Responsive Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, animateScroll as scroll } from 'react-scroll';
 import Home from './Home';
 import Footer from './Footer';
@@ -7,24 +7,29 @@ import About from './About';
 import Appointment from './Appointment';
 import './navbar.css'
 import { BsArrowUpCircleFill } from 'react-icons/bs';
+import { CgMenu } from 'react-icons/cg';
 import { useNavigate } from 'react-router-dom';
 
 
 function Navbar() {
+
+
+    const [showMenu, setShowMenu] = useState(false);
+
 
     const scrollToTop = () => {
         scroll.scrollToTop();
     };
     const navigate = useNavigate();
 
-    const handleBook=()=>{
+    const handleBook = () => {
         navigate('/slot')
     }
 
 
     return (
         <>
-            <div >
+            <div  >
                 <nav className="navbar">
 
                     <div > <h1>AiSense Naturopathy</h1></div>
@@ -84,10 +89,58 @@ function Navbar() {
                         </div>
 
                     </div>
+
+                    <div className='headerSM'>
+                        <button onClick={() => setShowMenu(!showMenu)} className='menuBTN'><CgMenu className='menuIcon' /></button>
+                        {showMenu && (
+                            <div className="sideDrawer">
+                                {/* Mobile Navigation Links */}
+                                <div className='homeTsm'>
+                                    <Link
+                                        activeClass="active"
+                                        to="home"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        <p>Home</p>
+                                    </Link>
+                                </div>
+                                <div className='homeTsm'>
+                                    <Link
+                                        activeClass="active"
+                                        to="about"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        <p>About us</p>
+                                    </Link>
+                                </div>
+                                <div className='homeTsm'>
+                                    <Link
+                                        activeClass="active"
+                                        to="contact"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        <p>Contact us</p>
+                                    </Link>
+                                </div>
+                                <div onClick={handleBook} className='bookSM'>
+                                    <p className='smBTN'>Book  Appointment</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </nav>
             </div>
 
-            <section id="home">
+            <section onClick={() => setShowMenu(false)} id="home">
                 <Home />
             </section>
             <section id="appointment">
@@ -95,14 +148,14 @@ function Navbar() {
             </section>
             <section id="about">
                 {/* <p>About</p> */}
-                <About/>
-             </section>
+                <About />
+            </section>
             <section id="contact">
                 {/* <p>Footer</p> */}
                 <Footer />
-             </section>
+            </section>
 
-            <button className='TopIcon' style={{border:'none',}} onClick={scrollToTop} ><BsArrowUpCircleFill /></button>
+            <button className='TopIcon' style={{ border: 'none', }} onClick={scrollToTop} ><BsArrowUpCircleFill /></button>
 
         </>
     )
